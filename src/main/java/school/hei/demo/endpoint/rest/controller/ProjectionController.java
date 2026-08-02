a0812fe16bfb74f8dec153d90a88a2b969bba48e
Tsiry <tsiryras3@gmail.com>
2026-08-03 00:36:09 +0300

    feat: projection controller

---
+++ src/main/java/school/hei/demo/endpoint/rest/controller/ProjectionController.java
@@ -0,0 +1,30 @@
+package school.hei.demo.endpoint.rest.controller;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import school.hei.demo.endpoint.rest.controller.dto.ProjectionRequest;
+import school.hei.demo.entity.Projection;
+import school.hei.demo.service.ProjectionService;
+@AllArgsConstructor
+@RestController
+@RequestMapping("/projections")
+
+public class ProjectionController {
+    private final ProjectionService projectionService;
+    @GetMapping
+    public List<Projection> getAllProjections() {
+        return projectionService.getAllProjections();
+    }
+    @PutMapping("/{id}")
+    public Projection updateProjection(@PathVariable UUID id, ProjectionRequest projectionRequest) {
+        return projectionService.updateProjection(id, projectionRequest);
+    }
+}
---
+++ src/main/java/school/hei/demo/service/ProjectionService.java
@@ -0,0 +1,24 @@
+package school.hei.demo.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import school.hei.demo.endpoint.rest.controller.dto.ProjectionRequest;
+import school.hei.demo.entity.Projection;
+
+/**
+ * ProjectionService
+ */
+public class ProjectionService {
+
+    public List<Projection> getAllProjections() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllProjections'");
+    }
+
+    public Projection updateProjection(UUID id, ProjectionRequest projectionRequest) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateProjection'");
+    }
+
+}
