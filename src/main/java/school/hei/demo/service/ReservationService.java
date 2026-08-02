53a0a7fb4be48176277cedc0b2d86b2b67e0e6be
Tsiry <tsiryras3@gmail.com>
2026-08-03 00:01:53 +0300

    feat: implement getReservation and getReservationById

--- src/main/java/school/hei/demo/service/ReservationService.java
+++ src/main/java/school/hei/demo/service/ReservationService.java
@@ -1,18 +1,33 @@
 package school.hei.demo.service;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
 import school.hei.demo.entity.Reservation;
-
+import school.hei.demo.exception.NotFoundException;
+import school.hei.demo.repository.ReservationRepository;
+import school.hei.demo.repository.mapper.ReservationMapper;
+@AllArgsConstructor
+@Service
 public class ReservationService {
+    private final ReservationRepository reservationRepository;
+    private final ReservationMapper reservationMapper;
 
-  public Reservation getReservation() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getReservation'");
+    
+  public List<Reservation> getReservation() {
+    return reservationRepository.findAll().stream()
+        .map(reservationMapper::toDomain)
+        .collect(Collectors.toList());
   }
 
   public Reservation getReservationById(UUID id) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getReservationById'");
+      return reservationRepository.findById(id)
+              .map(reservationMapper::toDomain)
+              .orElseThrow(() -> new NotFoundException("Reservation not found with id: " + id));
   }
 
   public Reservation updateReservation(UUID id) {
