d5078f6f06f44f7f10a4652167b0be8d30aa804d
Antsa Randria <antsaaa.randria@gmail.com>
2026-08-03 08:48:00 +0300

    feat: find by Id room implementation (#12)

--- src/main/java/school/hei/demo/service/RoomService.java
+++ src/main/java/school/hei/demo/service/RoomService.java
@@ -1,13 +1,19 @@
 package school.hei.demo.service;
 
 import java.util.UUID;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import school.hei.demo.exception.NotFoundException;
+import school.hei.demo.repository.RoomRepository;
 import school.hei.demo.repository.model.JRoom;
 
-/** RoomService */
+@AllArgsConstructor
+@Service
 public class RoomService {
 
+  private final RoomRepository repository;
+
   public JRoom findById(UUID roomId) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    return repository.findById(roomId).orElseThrow(() -> new NotFoundException("Room not found"));
   }
 }
