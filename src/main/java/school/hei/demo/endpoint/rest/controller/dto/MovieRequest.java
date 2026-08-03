bc93060f7ac30f42e061164b8bc33743667a9138
Ania-su <antsaaa.randria@gmail.com>
2026-08-03 08:06:00 +0300

    fix: format

--- src/main/java/school/hei/demo/endpoint/rest/controller/dto/MovieRequest.java
+++ src/main/java/school/hei/demo/endpoint/rest/controller/dto/MovieRequest.java
@@ -1,17 +1,16 @@
 package school.hei.demo.endpoint.rest.controller.dto;
 
-import lombok.Data;
-import school.hei.demo.entity.enums.Genre;
-
 import java.time.Duration;
 import java.util.List;
 import java.util.UUID;
+import lombok.Data;
+import school.hei.demo.entity.enums.Genre;
 
 @Data
 public class MovieRequest {
-    private UUID id;
-    private String title;
-    private List<Genre> genres;
-    private String description;
-    private Duration duration;
+  private UUID id;
+  private String title;
+  private List<Genre> genres;
+  private String description;
+  private Duration duration;
 }
--- src/main/java/school/hei/demo/service/MovieService.java
+++ src/main/java/school/hei/demo/service/MovieService.java
@@ -1,5 +1,7 @@
 package school.hei.demo.service;
 
+import java.util.List;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import school.hei.demo.endpoint.rest.controller.dto.MovieRequest;
@@ -9,49 +11,46 @@ import school.hei.demo.repository.MovieRepository;
 import school.hei.demo.repository.mapper.MovieMapper;
 import school.hei.demo.repository.model.JMovie;
 
-import java.util.List;
-import java.util.UUID;
-
 @Service
 @AllArgsConstructor
 public class MovieService {
 
-    private final MovieRepository movieRepository;
-    private final MovieMapper movieMapper;
+  private final MovieRepository movieRepository;
+  private final MovieMapper movieMapper;
 
-    public List<Movie> getMovies() {
-        return movieMapper.toDomain(movieRepository.findAll());
-    }
+  public List<Movie> getMovies() {
+    return movieMapper.toDomain(movieRepository.findAll());
+  }
 
-    public Movie getMovieById(UUID id) {
-        JMovie jMovie =
-                movieRepository.findById(id).orElseThrow(() -> new NotFoundException("Movie not found"));
-        return movieMapper.toDomain(jMovie);
-    }
+  public Movie getMovieById(UUID id) {
+    JMovie jMovie =
+        movieRepository.findById(id).orElseThrow(() -> new NotFoundException("Movie not found"));
+    return movieMapper.toDomain(jMovie);
+  }
 
-    public Movie createMovie(MovieRequest request) {
-        Movie toSave = new Movie();
-        toSave.setTitle(request.getTitle());
-        toSave.setGenres(request.getGenres());
-        toSave.setDescription(request.getDescription());
-        toSave.setDuration(request.getDuration());
-        return movieMapper.toDomain(movieRepository.save(movieMapper.toJpa(toSave)));
-    }
+  public Movie createMovie(MovieRequest request) {
+    Movie toSave = new Movie();
+    toSave.setTitle(request.getTitle());
+    toSave.setGenres(request.getGenres());
+    toSave.setDescription(request.getDescription());
+    toSave.setDuration(request.getDuration());
+    return movieMapper.toDomain(movieRepository.save(movieMapper.toJpa(toSave)));
+  }
 
-    public Movie updateMovie(UUID id, MovieRequest request) {
-        JMovie existing =
-                movieRepository.findById(id).orElseThrow(() -> new NotFoundException("Movie not found"));
-        existing.setTitle(request.getTitle());
-        existing.setGenres(request.getGenres());
-        existing.setDescription(request.getDescription());
-        existing.setDuration(request.getDuration());
-        return movieMapper.toDomain(movieRepository.save(existing));
-    }
+  public Movie updateMovie(UUID id, MovieRequest request) {
+    JMovie existing =
+        movieRepository.findById(id).orElseThrow(() -> new NotFoundException("Movie not found"));
+    existing.setTitle(request.getTitle());
+    existing.setGenres(request.getGenres());
+    existing.setDescription(request.getDescription());
+    existing.setDuration(request.getDuration());
+    return movieMapper.toDomain(movieRepository.save(existing));
+  }
 
-//    public void deleteMovie(UUID id) {
-//        if (!movieRepository.existsById(id)) {
-//            throw new NotFoundException("Movie not found");
-//        }
-//        movieRepository.deleteById(id);
-//    }
+  //    public void deleteMovie(UUID id) {
+  //        if (!movieRepository.existsById(id)) {
+  //            throw new NotFoundException("Movie not found");
+  //        }
+  //        movieRepository.deleteById(id);
+  //    }
 }
